585af61831a38b3f02c8111f2f572e263f1adad9
Ugur Tamer <UgurTamer1@Ugurs-MacBook-Air.local>
2017-11-15 21:23:42 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+function scrollToBottom(){
+  //selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child')
+  //height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
 
 socket.on('connect',function(){
   console.log('connected to server');
@@ -18,6 +33,7 @@ socket.on('newMessage', function(message){
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -29,6 +45,7 @@ socket.on('newLocationMessage', function(message){
     createdAt: formattedTime
   })
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 
